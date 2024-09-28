4a1cf329ad57234caae3b95582917d4480a3907a
jaimezzapata <zapataval2304@gmail.com>
2024-09-28 17:01:35 -0500

    Instalaciónd e json-server

--- src/pages/recipes/Vegetarian.jsx
+++ src/pages/recipes/Vegetarian.jsx
@@ -1,5 +1,6 @@
 import './Recipes.css'
 import { vegetarianRecipes } from '../../data/dataRecipes'
+
 const Vegetarian = () => {
   return (
     <div className="recipes">
@@ -7,10 +8,11 @@ const Vegetarian = () => {
       <section className="cards">
         {
           vegetarianRecipes.map((vegetarianRecipe) => (
-            <div>
+            <div className='card'>
               <h3>Nombre: {vegetarianRecipe.nombre}</h3>
               <p>Descripcion: {vegetarianRecipe.descripcion}</p>
               <p>Dificultad: {vegetarianRecipe.dificultad}</p>
+              <p>tiempo: {vegetarianRecipe.tiempo}</p>
               <p>tiempo: {vegetarianRecipe.tiempo}</p>
               <div>
                 <button>Eliminar</button>
