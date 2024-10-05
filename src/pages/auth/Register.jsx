5663002bf7f0672755da904987564fde6aedef80
jaimezzapata <zapataval2304@gmail.com>
2024-10-05 15:58:19 -0500

    Se capturan datos de registro

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,21 +1,33 @@
 import Header from "../../components/Header"
 import './Register.css'
+import { useState } from "react"
+let urlUsuarios = 'http://localhost:5273/usuarios'
+
 const Register = () => {
+
+    const [getUsuario, setUsuario] = useState('')
+    const [getPassword, setPassword] = useState('')
+    const [getEmail, setEmail] = useState('')
+
+    const createUser = () => {
+
+    }
+    
     return (
         <section>
             <Header />
             <form>
                 <section>
                     <label htmlFor="">Usuario</label>
-                    <input type="text" />
+                    <input onChange={(e) => { setUsuario(e.target.value) }} type="text" />
                 </section>
                 <section>
                     <label htmlFor="">Contraseña</label>
-                    <input type="password" />
+                    <input onChange={(e) => { setPassword(e.target.value) }} type="password" />
                 </section>
                 <section>
                     <label htmlFor="">Correo</label>
-                    <input type="email" />
+                    <input onChange={(e) => { setEmail(e.target.value) }} type="email" />
                 </section>
                 <button type="button">Registrar</button>
             </form>
